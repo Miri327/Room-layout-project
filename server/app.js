976a032f22b334c8ed3b74640628ed5eb7290001
Esti <st0548571544@gmail.com>
2026-04-14 17:26:43 +0300

    add mongodb connection

--- server/app.js
+++ server/app.js
@@ -1,12 +1,24 @@
+
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express()
+const PORT = process.env.PORT || 3001;
+const MONGO_URI = process.env.MONGO_URI;
+
+// חיבור למסד הנתונים MongoDB Atlas
+mongoose.connect(MONGO_URI)
+  .then(() => console.log('✅ Connected to MongoDB Atlas successfully!'))
+  .catch((err) => console.error('❌ MongoDB connection error:', err));
+
 
 app.get('/hello', (req, res) => {
     res.send('HELLO!!!😍👍👌😘🙌')
 })
 
-const port = 3001
-app.listen(port, () => {
-    console.log(`my app is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`my app is running on http://localhost:${PORT}`);
 })
